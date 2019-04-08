619ef1ef65551d61359207b4a85fbcd46c895671
wtq <1351158315@qq.com>
2019-04-08 21:37:15 +0800

    [改动]初始化代码提交

---
+++ common/src/main/java/com/sky/utils/str/UUIDUtils.java
@@ -0,0 +1,34 @@
+package com.sky.utils.str;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.UUID;
+
+/**
+ * 
+ * @author wangtq
+ * @time 2019年4月8日 上午9:38:55
+ * @description UUID生成工具类
+ *
+ */
+public class UUIDUtils {
+
+
+	/**
+	 * 32位UUID生成方法
+	 * 
+	 * @return 32位UUID生成方法
+	 */
+	public static String getUUID() {
+		UUID uuid = UUID.randomUUID();
+		String uuidStr = uuid.toString();
+		if (StringUtils.isNotEmpty(uuidStr)) {
+			uuidStr = uuidStr.toUpperCase();
+			uuidStr = uuidStr.replaceAll("-", "");
+		} else {
+			uuidStr = "";
+		}
+		return uuidStr;
+	}
+
+}
