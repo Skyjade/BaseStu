dea8048c20ba951f739882d31b917adb59c54bbc
wtq <1351158315@qq.com>
2019-04-28 17:47:37 +0800

    [改动]##

--- manager/src/main/java/com/sky/manager/app/PsnServerApplication.java
+++ manager/src/main/java/com/sky/manager/app/PsnServerApplication.java
@@ -2,11 +2,14 @@ package com.sky.manager.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication//(scanBasePackages = "com.hexing.miniscada")
+@SpringBootApplication(
+        exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})//(scanBasePackages = "com.sky")
 @ComponentScan(basePackages = {"com.sky.manager"})
 public class PsnServerApplication extends SpringBootServletInitializer {
 
